792c3776e54423f78cbe12e423e7c10982babe33
Stephanie <whatawhat@hotmail.com>
2021-08-10 14:59:02 -0400

    added space between newSchema so it was new Schema

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //create a new instance of the schema
-const workoutSchema = newSchema({
+const workoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
