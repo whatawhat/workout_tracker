4d66db9d48ab978128e1b4b863bbd01db7a1fd87
Stephanie <whatawhat@hotmail.com>
2021-08-11 10:53:01 -0400

    removed app use in hopes of fixing error

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerd
 });
 
 //Routes
-app.use(require("./routes/htmlRoutes")(app));
-app.use(require("./routes/apiRoutes")(app));
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
