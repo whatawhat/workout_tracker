624afd6000ce610ca370279438bd8c6f4c5e365c
Stephanie <whatawhat@hotmail.com>
2021-08-10 20:50:41 -0400

    created server.js file

---
+++ server.js
@@ -0,0 +1,34 @@
+//Import
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+//Express app
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//connects to database
+//database name is populate
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerdb", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+//Routes
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
