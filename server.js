82d5554a1ac26d5496ab1f64ae46dbd51a7fb357
Stephanie <whatawhat@hotmail.com>
2021-08-11 10:37:18 -0400

    updated routes

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerd
 });
 
 //Routes
-app.use(require("./routes/htmlRoutes"));
-app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes")(app));
+app.use(require("./routes/apiRoutes")(app));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
