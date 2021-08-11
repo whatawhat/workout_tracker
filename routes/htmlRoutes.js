1d8b3e6848b19509da0ee19d92b7b81570708e6a
Stephanie <whatawhat@hotmail.com>
2021-08-11 10:59:22 -0400

    trying to fix error message stating that dir name was not defined

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,17 @@
+//Express router to fix middleware issue 
+const router = require("express").Router();
 const path = require("path");
 
 module.exports = function(app) {
+    //home page
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(_dirname, "../public/index.html"));
+    });
+    //exercise page
     app.get("/exercise", (req, res) =>{
         res.sendFile(path.join(_dirname, "../public/exercise.html"));
     });
-
+    //stats page
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(_dirname, "../public/stats.html"));
     });
